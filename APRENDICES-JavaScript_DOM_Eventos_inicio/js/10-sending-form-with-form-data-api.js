ab4376f36d1e188f7a9262e771abb4328fec1f6a
CJCPDev <carmonacamilo53@gmail.com>
2026-02-04 21:55:27 -0500

    feat: Se agregaron ejercicios

---
+++ APRENDICES-JavaScript_DOM_Eventos_inicio/js/10-sending-form-with-form-data-api.js
@@ -0,0 +1,11 @@
+// Enviar un formulario con form data API
+
+
+const UserForm = document.querySelector('.formulario');
+
+UserForm.addEventListener('.submit', function (e){
+    e.preventDefault();
+
+
+
+});
